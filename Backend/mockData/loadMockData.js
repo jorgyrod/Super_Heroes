61eea83a7618ff9886b3b21a8a6c3aa576fd5510
jorgyrod <jorgsrodriguez@gmail.com>
2022-04-29 02:10:23 -0500

    Se agrega la opcion de utilizar una API para traer todos los paises y almacenarlos en nuestra base de datos

--- Backend/mockData/loadMockData.js
+++ Backend/mockData/loadMockData.js
@@ -4,6 +4,7 @@ const condition = require("./data/condition.json");
 const power = require("./data/power.json");
 const typeVehicles = require("./data/typeVehicles.json");
 const vehicles = require("./data/vehicles.json");
+const axios = require("axios");
 
 const {
   Category,
@@ -19,14 +20,29 @@ const {
 } = require("../src/db.js");
 
 const loadMockData = async () => {
-    console.log("Loading Mock Data...");
+  console.log("Loading Mock Data...");
 
-    await Report.bulkCreate(act);
-    //await Category.bulkCreate(categories);
-    //await Condition.bulkCreate(condition);
-    await Power.bulkCreate(power);
-    await Type.bulkCreate(typeVehicles);
-    await Vehicle.bulkCreate(vehicles);
-}
+  await Report.bulkCreate(act);
+  await Category.bulkCreate(categories);
+  await Condition.bulkCreate(condition);
+  await Power.bulkCreate(power);
+  await Type.bulkCreate(typeVehicles);
+  await Vehicle.bulkCreate(vehicles);
+  countriesAdd();
+};
 
 module.exports = loadMockData;
+
+const countriesAdd = async () => {
+  const countries = await axios.get(`https://restcountries.com/v3/all`);
+
+  await Promise.all(
+    countries.data.map((country) => {
+      let data = {
+          id: country.cca3,
+          nombre: country.name.common,
+      };
+      Country.findOrCreate({ where: data });
+    })
+  );
+};
